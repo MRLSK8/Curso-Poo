8a5520c37ebd954c549301ee1709c8ea01d8096b
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-02-18 15:15:06 -0300

    Aula14

---
+++ Aula14/src/aula14/Viewer.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula14;
+
+/**
+ *
+ * @author Aline
+ */
+public class Viewer extends Pessoa {
+    private String login; 
+    private int totalAssistido;
+
+    public Viewer(String nome, int idade, String sexo, String login) {
+        super(nome, idade, sexo);
+        this.login = login;
+        this.totalAssistido = 0;
+    }
+    
+    public void verMaisUm(){
+        this.totalAssistido += 1;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotalAssistido() {
+        return totalAssistido;
+    }
+
+    public void setTotalAssistido(int totalAssistido) {
+        this.totalAssistido = totalAssistido;
+    }
+
+    @Override
+    public String descricao() {
+        return super.descricao() + "login = " + login + ", totalAssistido = " + totalAssistido + "\n";
+    }
+    
+}
---
+++ Aula14/src/aula14/acoesVideo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula14;
+
+/**
+ *
+ * @author Aline
+ */
+public interface acoesVideo {
+    public abstract void play();
+    public void pause();
+    public void like();
+}
